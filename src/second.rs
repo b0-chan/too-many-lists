fec9f744bafec9d566c63ee2f0e19b3a3ab15ecc
bohwebo <osipenko226@gmail.com>
2021-07-07 15:20:52 +0300

    Improved linked-list

---
+++ src/second.rs
@@ -0,0 +1,204 @@
+#[derive(Debug)]
+struct List<T> {
+    head: Link<T>,
+}
+
+type Link<T> = Option<Box<Node<T>>>;
+
+#[derive(Debug)]
+struct Node<T> {
+    elem: T,
+    next: Link<T>,
+}
+
+impl<T> List<T> {
+    pub fn new() -> Self {
+        Self {
+            head: None
+        }
+    }
+
+    pub fn push(&mut self, elem: T) {
+        let node = Box::new(Node {
+            elem,
+            next: self.head.take(),
+        });
+
+        self.head = Some(node);
+    }
+
+    pub fn pop(&mut self) -> Option<T> {
+        self.head
+            .take()
+            .map(|node| {
+                self.head = node.next;
+                node.elem
+            })
+    }
+
+    pub fn peek(&self) -> Option<&T> {
+        self.head
+            .as_ref()
+            .map(|node| &node.elem)
+    }
+
+    pub fn peek_mut(&mut self) -> Option<&mut T> {
+        self.head.as_mut().map(|node| &mut node.elem)
+    }
+}
+
+impl<T> Drop for List<T> {
+    fn drop(&mut self) {
+        let mut curr = self.head.take();
+
+        while let Some(mut boxed_node) = curr {
+            curr = boxed_node.next.take();
+        }
+    }
+}
+
+struct IntoIter<T>(List<T>);
+
+struct Iter<'iter, T> {
+    next: Option<&'iter Node<T>>,
+}
+
+struct IterMut<'iter_mut, T> {
+    next: Option<&'iter_mut mut Node<T>>,
+}
+
+impl<T> List<T> {
+    pub fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
+    }
+
+    pub fn iter(&self) -> Iter<'_, T> {
+        Iter { next: self.head.as_deref() }
+    }
+
+    pub fn iter_mut(&mut self) -> IterMut<'_, T> {
+        IterMut { next: self.head.as_deref_mut() }
+    }
+}
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        // access fields of a tuple struct numerically
+        self.0.pop()
+    }
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.map(|node| {
+            self.next = node.next.as_deref();
+
+            &node.elem
+        })
+    }
+}
+
+impl<'iter_mut, T> Iterator for IterMut<'iter_mut, T> {
+    type Item = &'iter_mut mut T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next
+            .take()
+            .map(|node| {
+                self.next = node.next.as_deref_mut();
+                &mut node.elem
+            })
+    }
+}
+
+#[cfg(test)]
+#[test]
+fn insert_or_delete() {
+    let mut list = List::new();
+
+    // Check empty list behaves right
+    assert_eq!(list.pop(), None);
+
+    // Populate list
+    list.push(1);
+    list.push(2);
+    list.push(3);
+
+    assert_eq!(list.pop(), Some(3));
+
+    // Push some more just to make sure nothing's corrupted
+    list.push(4);
+    list.push(5);
+
+    // Check normal removal
+    assert_eq!(list.pop(), Some(5));
+    assert_eq!(list.pop(), Some(4));
+
+    // Check exhaustion
+    assert_eq!(list.pop(), Some(2));
+    assert_eq!(list.pop(), Some(1));
+}
+
+#[test]
+fn peek() {
+    let mut list = List::new();
+
+    assert_eq!(list.peek(), None);
+    assert_eq!(list.peek_mut(), None);
+
+    list.push(1);
+    list.push(2);
+    list.push(3);
+
+    assert_eq!(list.peek(), Some(&3));
+    assert_eq!(list.peek_mut(), Some(&mut 3));
+    list.peek_mut().map(|value| *value = 42);
+
+    // Check normal removal
+    assert_eq!(list.peek(), Some(&42));
+}
+
+#[test]
+fn into_iter() {
+    let mut list = List::new();
+    list.push(1);
+    list.push(2);
+    list.push(3);
+
+    let mut iter = list.into_iter();
+    assert_eq!(iter.next(), Some(3));
+    assert_eq!(iter.next(), Some(2));
+    assert_eq!(iter.next(), Some(1));
+    assert_eq!(iter.next(), None);
+}
+
+#[test]
+fn iter() {
+    let mut list = List::new();
+    list.push(1);
+    list.push(2);
+    list.push(3);
+
+    let mut iter = list.iter();
+    assert_eq!(iter.next(), Some(&3));
+    assert_eq!(iter.next(), Some(&2));
+    assert_eq!(iter.next(), Some(&1));
+    assert_eq!(iter.next(), None);
+}
+
+#[test]
+fn iter_mut() {
+    let mut list = List::new();
+    list.push(1);
+    list.push(2);
+    list.push(3);
+
+    let mut iter = list.iter_mut();
+    assert_eq!(iter.next(), Some(&mut 3));
+    assert_eq!(iter.next(), Some(&mut 2));
+    assert_eq!(iter.next(), Some(&mut 1));
+    assert_eq!(iter.next(), None);
+}
